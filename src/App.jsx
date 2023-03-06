3d53593df14944fe0e6ee8417054a275fa2d43db
Raycris Maldonado <raycris.maldonado@utreee.com>
2023-03-06 17:58:36 -0400

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+import Form from "./components/Form";
+import User from "./components/User";
+
+const API_URL = "https://api.github.com";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
 
   return (
-    <div >
-      <h1>We are looking for</h1>
+    <div>
+      <h2>Search GitHub User</h2>
+      <Form />
+      <h3>Results</h3>
+      <div id="results">
+        <div>
+          {results.map((user) => (
+            <User />
+          ))}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
